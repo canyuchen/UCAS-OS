f803af20c7a1d90db4c907ac253c49c097197fe4
harvardchen <harvardchen97@gmail.com>
2018-10-26 15:53:15 +0800

    not complete...

--- test/test_project2/test_scheduler1.c
+++ test/test_project2/test_scheduler1.c
@@ -36,7 +36,7 @@ void printk_task1(void)
         disable_interrupt();
 
         vt100_move_cursor(1, print_location);
-        printk("> [TASK] This task is to test scheduler. (%d)", i);
+        printf("> [TASK] This task is to test scheduler. (%d)", i);
 
         enable_interrupt();
 
